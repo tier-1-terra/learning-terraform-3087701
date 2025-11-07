df34d935e41cf5cdf167d6486fb33d73247c5100
tier-1-terra <tier-1@liceomibendicion.edu.gt>
2025-11-06 18:06:13 -0600

    corchetes

--- main.tf
+++ main.tf
@@ -52,14 +52,13 @@ module "alb" {
   subnets = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
 
-    http_tcp_listeners = [ 
+    http_tcp_listeners = 
       {
        port     = 80
        protocol = "HTTP"
        target_group_index = 0
-    }
-    ]
-    
+      }
+
   target_groups = {
     ex-instance = {
       name_prefix      = "blog-"
