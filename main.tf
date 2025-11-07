74e45d0e5b867d32bba157c1e8b4d4af17dae201
tier-1-terra <tier-1@liceomibendicion.edu.gt>
2025-11-06 18:12:40 -0600

    reparando todo

--- main.tf
+++ main.tf
@@ -52,26 +52,28 @@ module "alb" {
   subnets = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
 
-    http_tcp_listeners = 
+  target_groups = {
+      name_prefix      = "blog-"
+      protocol         = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+      targets = {
+        my_target = {
+        target_id      = "aws_instance.blog.id"
+        port = 80
+      }
+     }
+  http_tcp_listeners = 
       {
        port     = 80
        protocol = "HTTP"
        target_group_index = 0
       }
 
-  target_groups = {
-    ex-instance = {
-      name_prefix      = "blog-"
-      protocol         = "HTTP"
-      port             = 80
-      target_type      = "instance"
-      target_id        = "aws_instance.blog.id"
-    }
-  }
-
   tags = {
     Environment = "dev"
       }
+  }
 }
 
 module "blog_sg" {
